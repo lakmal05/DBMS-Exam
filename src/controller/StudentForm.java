39b18668eed7ffbf3563609505de59391019ed4e
lakmal05 <lakmaljayawardhana59@gmail.com>
2022-07-12 10:15:09 +0530

    create fiels in ui

--- src/controller/StudentForm.java
+++ src/controller/StudentForm.java
@@ -1,8 +1,27 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
 public class StudentForm {
 
 
+    public AnchorPane studentFormContext;
+    public TableView StudentTable;
+    
+    public TableColumn colStudentID;
+    public TableColumn colStudentName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
 
-
+    public JFXTextField txtStudentId;
+    public JFXTextField txtAddress;
+    public JFXTextField txtContact;
+    public JFXTextField txtEmail;
+    public JFXTextField txtStudentName;
+    public JFXTextField txtNic;
 }
---
+++ src/db/dataSet.java
@@ -0,0 +1,9 @@
+package db;
+
+import java.util.ArrayList;
+
+public class dataSet {
+    public static ArrayList<>studentTable =new ArrayList();
+
+
+}
